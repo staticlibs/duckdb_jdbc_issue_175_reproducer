5907e45041a14bd0ea38f2f8b87a928193bec313
Alex Kasko <alex@staticlibs.net>
2025-04-13 17:37:35 +0100

    revert to last reproducible version

--- Issue175Reproducer.java
+++ Issue175Reproducer.java
@@ -1,78 +1,64 @@
 import java.sql.*;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 
 public class Issue175Reproducer {
 
-    static class StickingSpinLock {
-        private final AtomicBoolean lock = new AtomicBoolean(false);
+    public static void main(String[] args) throws Exception {
+        int numCores = Runtime.getRuntime().availableProcessors();
+        int numShards = 3;
+        int numRows = 10_000_000;
+        int numConnThreads = numCores;
+        int numDbWorkerThreads = numCores;
 
-        void lock() {
-            while (!lock.compareAndSet(false, true)) {
-            }
+        System.out.println("CPU cores: " + numCores);
+        System.out.println("DB shards: " + numShards);
+        System.out.println("Connection threads: " + numConnThreads);
+        System.out.println("DB worker threads: " + numDbWorkerThreads);
+
+        TestConnPool connPool = new TestConnPool("test.db", numConnThreads, numDbWorkerThreads);
+        setupShards(connPool, numShards, numRows);
+        concurrentWrite(connPool, numShards, numConnThreads, numRows);
+    }
+
+    static void concurrentWrite(TestConnPool connPool, int numShards, int numConnThreads, int numRows)
+        throws Exception {
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        AtomicLong writeCount = new AtomicLong(0);
+        ExecutorService executorService = Executors.newFixedThreadPool(numConnThreads);
+        Random random = new Random();
+
+        System.out.println("Starting connection threads, count: " + numConnThreads);
+        for (int i = 0; i < numConnThreads; i++) {
+            executorService.submit(() -> {
+                while (true) {
+                    try {
+                        Connection conn = connPool.getConnection();
+                        int shardId = random.nextInt(numShards);
+                        int rowId = getNext(atomicInteger, numRows);
+                        executeQuery(conn, "update shard" + shardId +
+                                               ".main.test set amount = amount + 1 where id = " + rowId);
+                        conn.commit();
+                        writeCount.incrementAndGet();
+                        connPool.returnConnection(conn);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        System.exit(1);
+                    }
+                }
+            });
         }
 
-        void unlock() {
-            lock.set(false);
+        while (true) {
+            Thread.sleep(10000);
+            System.out.println("Write count: " + writeCount.get());
         }
     }
 
-    static class YieldingSpinLock {
-        private final AtomicBoolean lock = new AtomicBoolean(false);
-
-        void lock() {
-            while (!lock.compareAndSet(false, true)) {
-                Thread.yield();
-            }
-        }
-
-        void unlock() {
-            lock.set(false);
-        }
-    }
-
-    interface TestConnPool {
-        Connection takeConnection() throws Exception;
-
-        void returnConnection(Connection conn) throws Exception;
-    }
-
-    static class HikariConnPool implements TestConnPool {
-        final HikariDataSource hikariDataSource;
-
-        HikariConnPool(String dbPath, int numConnThreads, int numDbWorkerThreads) throws Exception {
-            HikariConfig hikariConfig = new HikariConfig();
-            hikariConfig.setJdbcUrl("jdbc:duckdb:" + dbPath);
-            hikariConfig.setMaximumPoolSize(numConnThreads * 10);
-            Properties driverConfig = new Properties();
-            driverConfig.put("threads", numDbWorkerThreads);
-            hikariConfig.setDataSourceProperties(driverConfig);
-            hikariConfig.setAutoCommit(false);
-            this.hikariDataSource = new HikariDataSource(hikariConfig);
-        }
-
-        @Override
-        public Connection takeConnection() throws Exception {
-            return hikariDataSource.getConnection();
-        }
-
-        @Override
-        public void returnConnection(Connection conn) throws Exception {
-            conn.close();
-        }
-    }
-
-    static void executeQuery(Connection connection, String query) throws Exception {
-        PreparedStatement statement = connection.prepareStatement(query);
-        statement.execute();
-        statement.close();
-    }
-
-    static int getNextRowId(AtomicInteger integer, int max) {
+    static int getNext(AtomicInteger integer, int max) {
         if (integer.get() >= max) {
             synchronized (integer) {
                 if (integer.get() >= max) {
@@ -85,69 +71,53 @@ public class Issue175Reproducer {
     }
 
     static void setupShards(TestConnPool connPool, int numShards, int numRows) throws Exception {
-        Connection conn = connPool.takeConnection();
+        Connection connection = connPool.getConnection();
         for (int i = 0; i < numShards; i++) {
             System.out.println("Generating data for shard number: " + i + ", rows count: " + numRows);
-            executeQuery(conn, "attach database 'shard" + i + ".db' as shard" + i);
-            executeQuery(conn, "use shard" + i);
-            executeQuery(conn, "create or replace table test (id bigint primary key, amount int, description varchar)");
-            executeQuery(conn, "insert into test" +
-                    " SELECT range as id, cast(random() * 100000 as bigint) as amount, repeat('x', 10) as description" +
-                    " FROM range(" + numRows + ");");
-            conn.commit();
+            executeQuery(connection, "attach database 'shard" + i + ".db' as shard" + i);
+            executeQuery(connection, "use shard" + i);
+            executeQuery(connection,
+                         "create or replace table test (id bigint primary key, amount int, description varchar)");
+            executeQuery(
+                connection,
+                "insert into test SELECT range id, cast(random() * 100000 as bigint) as amount, repeat('x', 10) as description FROM range(" +
+                    numRows + ");");
+            connection.commit();
         }
-        connPool.returnConnection(conn);
+        connPool.returnConnection(connection);
     }
 
-    static void concurrentWrite(TestConnPool connPool, int numShards, int numConnThreads, int numRows) throws Exception {
-        AtomicLong writeCount = new AtomicLong(0);
-        AtomicInteger atomicInteger = new AtomicInteger(0);
-        Random random = new Random();
-
-        System.out.println("Starting connection threads, count: " + numConnThreads);
-        for (int i = 0; i < numConnThreads; i++) {
-            Thread th = new Thread(() -> {
-                while (true) {
-                    try  {
-                        Connection connection = connPool.takeConnection();
-
-                        int shardId = random.nextInt(numShards) % numShards;
-                        int rowId = getNextRowId(atomicInteger, numRows);
-
-                        executeQuery(connection, "update shard" + shardId + ".main.test set amount = amount + 1 where id = " + rowId);
-                        connection.commit();
-                        writeCount.incrementAndGet();
-
-                        connPool.returnConnection(connection);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        System.exit(1);
-                    }
-                }
-            });
-            th.start();
-        }
-
-        while (true) {
-            Thread.sleep(10000);
-            System.out.println("Write count: " + writeCount.get());
+    static void executeQuery(Connection connection, String query) throws Exception {
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.execute();
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        int numCores = Runtime.getRuntime().availableProcessors();
-        int numShards = 3;
-        int numConnThreads = 10;
-        int numDbWorkerThreads = numCores;
-        int numRows = 10_000_000;
+    static class TestConnPool {
+        final List<Connection> connections = new ArrayList<>();
+        final Random random = new Random();
 
-        System.out.println("CPU cores: " + numCores);
-        System.out.println("DB shards: " + numShards);
-        System.out.println("Connection threads: " + numConnThreads);
-        System.out.println("DB worker threads: " + numDbWorkerThreads);
+        TestConnPool(String dbPath, int numConnThreads, int numDbWorkerThreads) throws Exception {
+            Properties config = new Properties();
+            config.put("threads", numDbWorkerThreads);
+            for (int i = 0; i < numConnThreads; i++) {
+                Connection conn = DriverManager.getConnection("jdbc:duckdb:" + dbPath, config);
+                conn.setAutoCommit(false);
+                connections.add(conn);
+            }
+        }
 
-        TestConnPool connPool = new HikariConnPool("test.db", numConnThreads, numDbWorkerThreads);
-        setupShards(connPool, numShards, numRows);
-        concurrentWrite(connPool, numShards, numConnThreads, numRows);
+        public Connection getConnection() {
+            synchronized (this) {
+                int idx = random.nextInt(connections.size());
+                return connections.remove(idx);
+            }
+        }
+
+        public void returnConnection(Connection conn) {
+            synchronized (this) {
+                connections.add(conn);
+            }
+        }
     }
 }
